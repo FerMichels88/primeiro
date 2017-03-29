4f563ff3dcaac8119a19eebaf231e2c50fcc6b43
fernando <michelsdc8@gmail.com>
2017-03-28 21:42:27 -0300

    inclusao de cod

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const { soma } = require('./calc');
 
 console.log(soma(2, 5));
+
+console.log(soma(7, 7));
+
+console.log(soma(150, 450));
+
+console.log(soma(37, 5));
+
+console.log(divisao(20, 2));
