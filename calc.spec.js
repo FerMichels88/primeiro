564fa9a0563c16761ff7d1ee9c2bcbcb6c43801d
fernando <michelsdc8@gmail.com>
2017-03-28 21:18:04 -0300

    inclusao de novos testes

--- calc.spec.js
+++ calc.spec.js
@@ -9,3 +9,27 @@ lab.test('retorno da soma de 2 e 3 deve ser 5', (done) => {
     Code.expect(soma(2, 3)).to.equal(5);
     done();
 });
+
+lab.test('retorno da soma de 7 e 7 deve ser 14', (done) => {
+
+    Code.expect(soma(7, 7)).to.equal(14);
+    done();
+});
+
+lab.test('retorno da soma de 150 e 300 deve ser 450', (done) => {
+
+    Code.expect(soma(150, 350)).to.equal(450);
+    done();
+});
+
+lab.test('retorno da soma de 37 e a deve ser 37', (done) => {
+
+    Code.expect(soma(37, a)).to.equal(37);
+    done();
+});
+
+lab.test('retorno da divisao de 20 com 2 deve ser 10', (done) => {
+
+    Code.expect(soma(20, 2)).to.equal(10);
+    done();
+});
